87e0191a0dbc241b80d2c94c792c55cb06fe14ee
caisilus <serangor@gmail.com>
2022-12-26 11:41:52 +0300

    Add dummy confidence values

---
+++ storage/filler.py
@@ -0,0 +1,12 @@
+﻿def fill_with_05(infile, outfile):
+    with open(infile, encoding="UTF-8") as without_confidence:
+        with open(outfile, "w", encoding="UTF-8") as with_confidence:
+            for line in without_confidence:
+                stripped = line.strip()
+                if stripped != "" and not stripped.startswith("-"):
+                    with_confidence.write(stripped + "0.5;\n")
+                else:
+                    with_confidence.write(line)
+                    
+fill_with_05("Facts_new.txt", "Facts_with_confidence.txt")
+fill_with_05("Rules.txt", "Rules_with_confidence.txt")
